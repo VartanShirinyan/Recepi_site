ccb479feb37e3f4bceb922462956892b48af83b4
Vartan Shirinyan <02vartan-s@mail.ru>
2025-03-02 16:35:38 +0300

    Исправления

--- recipe_site/settings.py
+++ recipe_site/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'recipes/static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'recipes/static')] #
 
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
